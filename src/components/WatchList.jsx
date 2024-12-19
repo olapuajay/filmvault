1e37dea6c991e0f92c7238ca12624041a6520912
Olapu Ajay <olapuajay@gmail.com>
2024-12-19 22:17:16 +0530

    enhance WatchList component with search input and table structure for movie display

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 function WatchList() {
   return (
-    <div>
-      This is the WatchList component
-    </div>
+    <>
+      <div className="form-group text-center w-50 mx-auto mt-4 mb-4">
+        <input type="text" className='form-control text-dark' placeholder='Search for movies' />
+      </div>
+      <div className='container mt-4'>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ratings</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
