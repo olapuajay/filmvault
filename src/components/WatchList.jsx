af3071492a5c8f6267f05b0550d3cc28b4009685
Olapu Ajay <olapuajay@gmail.com>
2024-12-22 11:07:22 +0530

    add search functionality and enhance table structure in WatchList component

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faT } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function WatchList({watchlist}) {
+  const [search, setSearch] = useState('')
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
   return (
-    <>
+    <div className='container mt-4'>
       <h3 className='text-center fw-bold text-uppercase mt-3'>Watch List</h3>
       <div className='container mt-4 d-flex justify-content-center gap-4'>
         <button className='btn btn-primary'> Action </button>
         <button className='btn btn-light'> Adventure </button>
       </div>
-      <div className="form-group text-center w-50 mx-auto mt-4 mb-4">
-        <input type="text" className='form-control text-dark' placeholder='Search for movies' />
+      <div className="form-group text-center mx-auto mt-4 mb-4">
+        <input onChange={handleSearch} value={search} type="text" className='form-control text-dark' placeholder='Search Movies' />
       </div>
       <div className='container mt-4'>
-        <table className='table table-striped table-hover'>
+        <table className='table table-striped table-hover container-fluid'>
           <thead>
             <tr>
               <th>Name</th>
-              <th>Ratings</th>
+              <th className='d-flex justify-content-between'>
+                <div> <FontAwesomeIcon icon={faArrowUp} /> </div>
+                <div>Ratings</div>
+                <div> <FontAwesomeIcon icon={faArrowDown} /> </div>
+              </th>
               <th>Popularity</th>
               <th>Genre</th>
             </tr>
           </thead>
           <tbody>
             {
-              watchlist.map((movieObj) => {
+              watchlist.filter((movieObj) => {
+                return movieObj.title.toLowerCase().includes(search.toLowerCase())
+              }).map((movieObj) => {
                 return <tr className='align-middle'>
                   <td>
                     <img src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`} alt="Movie Poster" style={{width: '3rem'}} />
@@ -32,14 +46,16 @@ function WatchList({watchlist}) {
                   <td> {movieObj.vote_average} </td>
                   <td> {movieObj.popularity} </td>
                   <td>Adventure</td>
-                  <td className='text-danger'>Delete</td>
+                  <td className='text-danger'>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </td>
                 </tr>
               })
             }
           </tbody>
         </table>
       </div>
-    </>
+    </div>
   )
 }
 
