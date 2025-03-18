d7ccc403f4139ee5739772fbc4838f116566766a
Olapu Ajay <olapuajay@gmail.com>
2025-03-18 11:11:43 +0530

    refactor WatchList component: enhance layout with responsive design, improve button styling, and update table structure for better readability

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -40,14 +40,14 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
   return (
     <div className="mt-16">
       <h3 className="text-center text-white text-xl font-bold">Watch List</h3>
-      <div className="mt-4 flex justify-center gap-4">
+      <div className="mt-4 flex flex-wrap justify-center gap-2">
         {genreList.map((genre, index) => {
           return (
             <button
               key={index}
               onClick={() => handleFilter(genre)}
               className={
-                currGenre === genre ? "bg-sky-400 px-4 py-2 rounded-lg text-sm text-white font-semibold" : "bg-gray-800 py-2 px-4 rounded-lg text-sm text-white"
+                currGenre === genre ? "bg-sky-400 md:px-4 md:py-2 p-2 rounded-lg md:text-sm text-xs text-white font-semibold" : "bg-gray-800 md:py-2 md:px-4 p-2 rounded-lg md:text-sm text-xs text-white"
               }
             >
               {" "}
@@ -61,7 +61,7 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
           onChange={handleSearch}
           value={search}
           type="text"
-          className="p-2 rounded-lg md:w-1/2 border-2 border-gray-300 bg-gray-800 text-white active:outline-none focus:outline-none"
+          className="p-2 rounded-xl md:w-1/2 border-2 border-gray-300 bg-gray-800 text-white active:outline-none focus:outline-none"
           placeholder="Search Movies"
         />
       </div>
@@ -70,12 +70,12 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
           <div className="text-center text-gray-300 text-lg">No Movies in Watchlist</div> 
         )}
       </div>
-      <div className="mx-auto my-4">
-        <table className="table-auto text-white mx-auto w-3/4">
+      <div className="overflow-x-auto">
+        <table className="table-auto text-white mx-auto min-w-full">
           <thead>
             <tr>
-              <th>Name</th>
-              <th className="flex justify-center items-center gap-2">
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2 flex justify-center items-center gap-2">
                 <div onClick={sortDecrease}>
                   <ArrowUp size={20} />
                 </div>
@@ -84,8 +84,8 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
                   <ArrowDown size={20} />
                 </div>
               </th>
-              <th>Popularity</th>
-              <th>Genre</th>
+              <th className="px-4 py-2">Popularity</th>
+              <th className="px-4 py-2">Genre</th>
             </tr>
           </thead>
           <tbody>
@@ -104,8 +104,8 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
               })
               .map((movieObj) => {
                 return (
-                  <tr className="border-b-2 border-gray-800 py-2">
-                    <td className="flex items-center gap-2 my-2">
+                  <tr key={movieObj.id} className="border-b-2 border-gray-800 py-2">
+                    <td className="flex items-center gap-2 my-2 px-4 py-2">
                       <img
                         src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
                         alt="Movie Poster"
@@ -113,12 +113,12 @@ function WatchList({ watchlist, setWatchlist, handleRemoveFromWatchlist }) {
                       />
                       <p className="text-gray-300"> {movieObj.title} </p>
                     </td>
-                    <td className="text-center text-gray-300"> {movieObj.vote_average} </td>
-                    <td className="text-center text-gray-300"> {movieObj.popularity} </td>
-                    <td className="text-center text-gray-300"> {genreIds[movieObj.genre_ids[0]]} </td>
+                    <td className="text-center text-gray-300 px-4 py-2"> {movieObj.vote_average} </td>
+                    <td className="text-center text-gray-300 px-4 py-2"> {movieObj.popularity} </td>
+                    <td className="text-center text-gray-300 px-4 py-2"> {genreIds[movieObj.genre_ids[0]]} </td>
                     <td
                       onClick={() => handleRemoveFromWatchlist(movieObj)}
-                      className="text-red-500 cursor-pointer"
+                      className="text-red-500 cursor-pointer px-4 py-2"
                     >
                       <Trash size={20} />
                     </td>
